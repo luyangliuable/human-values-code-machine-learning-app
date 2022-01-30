159453ea237272159c9cb42a49bca7ee9cc9e960
Luyang <rend1820@pop-os.localdomain>
2022-01-30 23:20:45 +1100

    Linked task to heroku redis

--- project/server/tasks.py
+++ project/server/tasks.py
@@ -8,8 +8,9 @@ from celery import Celery
 from project.machine_learning import app as machine_learning
 
 celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:8888")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:8888")
+celery.conf.broker_url = os.environ["redis://:pbde7018282c5cc3afc21b12012e4eb52ee56eda5dd96c819113b3e7694dabe13@ec2-35-168-186-135.compute-1.amazonaws.com:27890"]
+celery.conf.celery_task_serializer = 'json'
+celery.conf.result_backend = os.environ["redis://:pbde7018282c5cc3afc21b12012e4eb52ee56eda5dd96c819113b3e7694dabe13@ec2-35-168-186-135.compute-1.amazonaws.com:27890"]
 
 
 @celery.task(name="create_task")
