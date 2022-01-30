9459da5020bdd8f56edbfa98644833f84c2ba893
Luyang <rend1820@pop-os.localdomain>
2022-01-30 23:36:04 +1100

    Attempt to fix heroku memory quote being  exceeded

--- project/server/tasks.py
+++ project/server/tasks.py
@@ -9,7 +9,7 @@ from project.machine_learning import app as machine_learning
 
 celery = Celery(__name__)
 celery.conf.broker_url ="redis://:pbde7018282c5cc3afc21b12012e4eb52ee56eda5dd96c819113b3e7694dabe13@ec2-35-168-186-135.compute-1.amazonaws.com:27890"
-celery.conf.celery_task_serializer = 'json'
+# celery.conf.celery_task_serializer = 'json'
 celery.conf.result_backend = "redis://:pbde7018282c5cc3afc21b12012e4eb52ee56eda5dd96c819113b3e7694dabe13@ec2-35-168-186-135.compute-1.amazonaws.com:27890"
 
 
